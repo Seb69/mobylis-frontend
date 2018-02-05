546c8c0a4ca1e08d2a259ef66d8785fdeffb4305
Sebastien <sebastien.andre.seb@gmail.com>
2018-02-05 20:00:18 +0100

    update node to 8.9.4

--- src/app/carousel/carousel/carousel.component.ts
+++ src/app/carousel/carousel/carousel.component.ts
@@ -143,7 +143,6 @@ export class CarouselComponent implements OnInit {
             this.loadImage = loadImageTemp;
             this.ref.detectChanges();
           }, 300);
-
         }
       );
     }
